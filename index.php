12806bd1608b3a4dfe34631a2b8112aa84bfe3c8
Monica Danielle <monicadanielle.estrella.lazarte@apcas.ph.education>
2021-07-07 12:41:23 -0800

    Added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//display
+  echo "<p><center>MONICA DANIELLE E. LAZARTE</center></p>";
+  echo "</br>";
+  echo "<p><center>BASIC CALCULATOR</center></p>";
+  echo "</br>";
+  echo "<p><center>First number: 5</center></p>";
+  echo "</br>";
+  echo "<p><center>Second number: 5</center></p>";
+  echo "</br>";
+  echo '_____________________________________________________________________________________________________________________________________________________________________________';
+
+  //
+  echo "</br>";
+  echo "Addition = 10";
+  echo "</br>";
+  echo "Subtraction = 0";
+  echo "</br>";
+  echo "Multiplication = 25";
+  echo "</br>";
+  echo "Division = 1";
+ 
+  ?>
